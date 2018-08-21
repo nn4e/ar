8320a9e3198a32a8c9a5701c214a89a58b5b1041
Алексей <support@prowebmake.ru>
2018-08-21 18:52:07 +0300

    model 2 center

--- js/main.js
+++ js/main.js
@@ -429,14 +429,15 @@ function load() {
     var loader2 = new THREE.OBJLoader();
     loader2.load('data/model_2.obj', function(obj) {
 
-        obj.scale.set(0.0002, 0.0002, 0.0002);
+        obj.scale.set(0.0003, 0.0003, 0.0003);
 
         obj.children[0].material = [
             new THREE.MeshBasicMaterial({ color: 0xF0FF00, side: 2 }),
             new THREE.MeshBasicMaterial({ color: 0xFFAE4F, side: 2 })
         ];
 
-        obj.position.z = -0.2;
+        //obj.position.z = -0.3;
+        obj.children[0].geometry.center();
 
         var object = new THREE.Group();
         var object2 = new THREE.Group();
